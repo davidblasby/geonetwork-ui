6aef34cd86b5ab5b15a8f62a5f1ed2b29792347f
david.blasby <david.blasby@geocat.net>
2023-06-12 14:39:03 -0700

    from florent - make srue that FeatureSearchModule is initialized with 'constructor(private service: FieldsService)' and that we tag this as an app ('tags' in project.json)

--- apps/my-app/src/app/app.component.ts
+++ apps/my-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core'
+import { FieldsService } from '@geonetwork-ui/feature/search'
 
 @Component({
   selector: 'geonetwork-ui-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core'
 })
 export class AppComponent {
   title = 'my-app'
+
+
+  constructor(private service: FieldsService) {}
+
+
 }
